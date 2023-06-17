89449e6a658ca82853675829c6231b547490ba9a
winder2412 <winderdawshmaster@gmail.com>
2023-06-17 10:59:10 +0330

    June 17th

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -11,7 +11,8 @@ else
     yay --version
     cd ..
 fi
-yay -S --noconfirm zsh zsh-syntax-highlighting autojump zsh-autosuggestions
+yay -S --noconfirm zsh zsh-syntax-highlighting autojump zsh-autosuggestions git 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/powerlevel10k
 ##copy the config file
 cp -rf ./zsh ~/.config/zsh
 cp -f ./.zshenv ~
