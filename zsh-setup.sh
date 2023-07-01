a508eb1f99a096838d2ea3507a2b0c56411cdefa
winder2412 <winderdawshmaster@gmail.com>
2023-07-01 14:23:48 +0330

    fix yay problem

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-##installing the needed packeges
-if command -v apt >/dev/null 2>&1; then
-	sudo apt update
-	sudo apt install -y zsh git
-elif command -v yay >/dev/null 2>&1; then
-	yay -Sy --noconfirm zsh git
-elif command -v dnf >/dev/null 2>&1; then
-	sudo dnf install -y zsh git
+
+os=$(uname -a)
+
+if [[ $os == *"arch"* ]]; then
+  # Install zsh using pacman package manager
+  sudo pacman -Sy zsh git
+elif [[ $os == *"debian"* ]]; then
+  # Install zsh using apt package manager
+  sudo apt-get install zsh git
+elif [[ $os == *"fedora"* ]]; then
+  # Install zsh using dnf package manager
+  sudo dnf install zsh git
 else
-	echo "None of the required package managers (apt, yay, dnf) are available."
-	exit 1
+  echo "Unsupported operating system"
 fi
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ./zsh/.zsh/zsh-autosuggestions
